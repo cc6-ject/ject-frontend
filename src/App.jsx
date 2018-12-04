27b6352de9a0b37122d52388e2f8eda23aa99dfe
mp40 <philps.matthew@gmail.com>
2018-12-04 11:44:37 +0900

    material-ui added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import "./App.css";
 
+import Button from "@material-ui/core/Button";
+
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -13,6 +15,11 @@ class App extends React.Component {
         <header className="App-header">
           <div className="Wrapper">
             <div className="Title">*Mock Up*</div>
+            <div>
+              <Button variant="contained" color="primary">
+                test
+              </Button>
+            </div>
           </div>
         </header>
       </div>
