900cfa14e33849c7fcb481da51916cb0a81e4172
mp40 <philps.matthew@gmail.com>
2018-12-04 10:58:08 +0900

    rough boilerplate done

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <div className="Wrapper">
+            <div className="Title">*Mock Up*</div>
+          </div>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
