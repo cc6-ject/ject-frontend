b370155d68c42463a0aa1a1620b2d66eb36ca9b4
Daenam Kim <daenam.kim@gmail.com>
2018-12-11 09:46:52 +0900

    Refactor HomePage.

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -1,8 +1,12 @@
-import React, { Component } from "react";
-
-import { withStyles } from "@material-ui/core/styles";
-import Paper from "@material-ui/core/Paper";
-import Grid from "@material-ui/core/Grid";
+import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import {
+  Button,
+  GridList,
+  GridListTile,
+  GridListTileBar
+} from '@material-ui/core';
+import { menus } from './Constants';
 
 const styles = theme => ({
   root: {
@@ -10,64 +14,74 @@ const styles = theme => ({
   },
   paper: {
     padding: theme.spacing.unit * 2,
-    textAlign: "center",
+    textAlign: 'center',
     color: theme.palette.text.secondary,
     width: 500
+  },
+  gridRoot: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+    overflow: 'hidden',
+    backgroundColor: theme.palette.background.paper
+  },
+  gridList: {
+    width: 200,
+    hei1ght: 200
+  },
+  gridIcon: {
+    color: 'rgba(255, 255, 255, 0.54)'
   }
 });
 
-class HomePage extends Component {
-  render() {
-    const { classes } = this.props;
-    return (
-      <div>
-        <Grid
-          container
-          direction="column"
-          justify="flex-start"
-          alignItems="center"
-          spacing={24}
-        >
-          <Grid item xs={4}>
-            <Paper
-              className={classes.paper}
-              onClick={this.props.toggleState.bind(
-                this,
-                "Projection Practice",
-                "Projection"
-              )}
-            >
-              Practice Projection
-            </Paper>
-          </Grid>
-          <Grid item xs={4}>
-            <Paper
-              className={classes.paper}
-              onClick={this.props.toggleState.bind(
-                this,
-                "Tongue Twisters",
-                "TongueTwister"
-              )}
-            >
-              Tounge Twisters
-            </Paper>
-          </Grid>
-          <Grid item xs={4}>
-            <Paper
-              className={classes.paper}
-              onClick={this.props.toggleState.bind(
-                this,
-                "Challenge Mode",
-                "Challenge"
-              )}
-            >
-              Challenge Mode
-            </Paper>
-          </Grid>
-        </Grid>
-      </div>
-    );
+const tileData = [
+  {
+    img: menus.projection.IMAGE,
+    title: menus.projection.TITLE,
+    description: 'Description of Practice Projection...'
+  },
+  {
+    img: menus.tongueTwister.IMAGE,
+    title: menus.tongueTwister.TITLE,
+    description: 'Description of Tongue Twisters...'
+  },
+  {
+    img: menus.challenge.IMAGE,
+    title: menus.challenge.TITLE,
+    description: 'Description of Challenge Mode...'
+  },
+  {
+    img: menus.karaoke.IMAGE,
+    title: menus.karaoke.TITLE,
+    description: 'Description of Karaoke...'
   }
-}
+];
+
+const HomePage = ({ classes, switchMenu }) => (
+  <div classes={classes.gridRoot}>
+    <GridList
+      cellHeight={400}
+      classes={classes.gridList}
+      style={{ paddingLeft: 20, paddingRight: 20 }}
+    >
+      {tileData.map((tile, index) => (
+        <GridListTile key={index}>
+          <Button
+            style={{ padding: 0 }}
+            onClick={() => {
+              switchMenu(tile.title);
+            }}
+          >
+            <img src={tile.img} alt={tile.title} />
+          </Button>
+          <GridListTileBar
+            title={tile.title}
+            subtitle={<span>{tile.description}</span>}
+          />
+        </GridListTile>
+      ))}
+    </GridList>
+  </div>
+);
 
 export default withStyles(styles)(HomePage);
