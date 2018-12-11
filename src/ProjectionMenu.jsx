5ff16d23c9f6fdf63847ab1b47d6c4649d176a55
Daenam Kim <daenam.kim@gmail.com>
2018-12-11 18:32:38 +0900

    Add more view files.

--- src/ProjectionMenu.jsx
+++ src/ProjectionMenu.jsx
@@ -1,8 +1,8 @@
-import React, { Component } from "react";
-import { withStyles } from "@material-ui/core/styles";
-import Fab from "@material-ui/core/Fab";
-import { API, Auth } from "aws-amplify";
-import "./ProjectionMenu.css";
+import React, { Component } from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import { Fab, Card, CardContent } from '@material-ui/core';
+import { API, Auth } from 'aws-amplify';
+import './ProjectionMenu.css';
 
 const styles = theme => ({
   fab: {
@@ -20,12 +20,12 @@ class ProjectionMenu extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      volume: { transform: "rotate(0deg)" },
+      volume: { transform: 'rotate(0deg)' },
       isTry: false,
       intervalID: null,
       trainingDecibel: [],
       avgDecibels: [],
-      username: ""
+      username: ''
     };
   }
 
@@ -35,7 +35,7 @@ class ProjectionMenu extends Component {
       this.setState({ username: data.id });
       console.log(data);
     } catch (e) {
-      alert(e.message);
+      // alert(e.message);
     }
   }
 
@@ -43,6 +43,7 @@ class ProjectionMenu extends Component {
     const constraint = { audio: true };
     navigator.getUserMedia(constraint, this.handleSuccess, this.handleError);
   };
+
   handleSuccess = stream => {
     const id = setInterval(() => {
       this.setState({
@@ -63,7 +64,7 @@ class ProjectionMenu extends Component {
     microphone.connect(analyser);
 
     // Pass microphone data to processor
-    const processor = audioContext.createScriptProcessor(8192, 1, 1);
+    const processor = audioContext.createScriptProcessor(1024, 1, 1);
     analyser.connect(processor);
     processor.connect(audioContext.destination);
 
@@ -72,11 +73,12 @@ class ProjectionMenu extends Component {
       const array = new Uint8Array(analyser.frequencyBinCount);
       analyser.getByteFrequencyData(array);
       average = getAverageVolume(array);
-      console.log("VOLUME:" + average);
+      console.log('VOLUME:' + average);
     };
   };
+
   handleError = err => {
-    console.log("The following error occured: " + err.name);
+    console.log('The following error occured: ' + err.name);
   };
 
   handleClose = async () => {
@@ -100,9 +102,10 @@ class ProjectionMenu extends Component {
     });
     clearInterval(state.intervalID);
   };
+
   saveToAWS = (trainingDecibel, avgDecibel) => {
-    console.log("HIA");
-    return API.post("ject", "/decibel", {
+    console.log('HIA');
+    return API.post('ject', '/decibel', {
       body: { decibel: trainingDecibel, avgDecibel: avgDecibel },
       requestContext: {
         identity: {
@@ -122,55 +125,59 @@ class ProjectionMenu extends Component {
     const { classes } = this.props;
     return (
       <div className="Projection">
-        <svg
-          className="ThumbCheck"
-          xmlns="http://www.w3.org/2000/svg"
-          width="200"
-          height="200"
-          viewBox="0 0 24 24"
-          style={this.state.volume}
-        >
-          <path d="M1 21h4V9H1v12zm22-11c0-1.1-.9-2-2-2h-6.31l.95-4.57.03-.32c0-.41-.17-.79-.44-1.06L14.17 1 7.59 7.59C7.22 7.95 7 8.45 7 9v10c0 1.1.9 2 2 2h9c.83 0 1.54-.5 1.84-1.22l3.02-7.05c.09-.23.14-.47.14-.73v-1.91l-.01-.01L23 10z" />
-        </svg>
-        <br />
-        <br />
-        <br />
-
-        {!this.state.isTry ? (
-          <Fab
-            color="secondary"
-            className={classes.fab}
-            onClick={() => {
-              this.handleClick();
-            }}
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-            >
-              <path d="M12 14c1.66 0 2.99-1.34 2.99-3L15 5c0-1.66-1.34-3-3-3S9 3.34 9 5v6c0 1.66 1.34 3 3 3zm5.3-3c0 3-2.54 5.1-5.3 5.1S6.7 14 6.7 11H5c0 3.41 2.72 6.23 6 6.72V21h2v-3.28c3.28-.48 6-3.3 6-6.72h-1.7z" />
-            </svg>
-          </Fab>
-        ) : (
-          <Fab
-            color="secondary"
-            className={classes.fab}
-            onClick={() => {
-              this.handleClose();
-            }}
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-            >
-              <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.42 0-8-3.58-8-8 0-1.85.63-3.55 1.69-4.9L16.9 18.31C15.55 19.37 13.85 20 12 20zm6.31-3.1L7.1 5.69C8.45 4.63 10.15 4 12 4c4.42 0 8 3.58 8 8 0 1.85-.63 3.55-1.69 4.9z" />
-            </svg>
-          </Fab>
-        )}
+        <Card style={{ textAlign: 'center', margin: 100 }}>
+          <CardContent>
+            <p>
+              <svg
+                className="ThumbCheck"
+                xmlns="http://www.w3.org/2000/svg"
+                width="200"
+                height="200"
+                viewBox="0 0 24 24"
+                style={this.state.volume}
+              >
+                <path d="M1 21h4V9H1v12zm22-11c0-1.1-.9-2-2-2h-6.31l.95-4.57.03-.32c0-.41-.17-.79-.44-1.06L14.17 1 7.59 7.59C7.22 7.95 7 8.45 7 9v10c0 1.1.9 2 2 2h9c.83 0 1.54-.5 1.84-1.22l3.02-7.05c.09-.23.14-.47.14-.73v-1.91l-.01-.01L23 10z" />
+              </svg>
+            </p>
+            <p>
+              {!this.state.isTry ? (
+                <Fab
+                  color="secondary"
+                  className={classes.fab}
+                  onClick={() => {
+                    this.handleClick();
+                  }}
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M12 14c1.66 0 2.99-1.34 2.99-3L15 5c0-1.66-1.34-3-3-3S9 3.34 9 5v6c0 1.66 1.34 3 3 3zm5.3-3c0 3-2.54 5.1-5.3 5.1S6.7 14 6.7 11H5c0 3.41 2.72 6.23 6 6.72V21h2v-3.28c3.28-.48 6-3.3 6-6.72h-1.7z" />
+                  </svg>
+                </Fab>
+              ) : (
+                <Fab
+                  color="secondary"
+                  className={classes.fab}
+                  onClick={() => {
+                    this.handleClose();
+                  }}
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.42 0-8-3.58-8-8 0-1.85.63-3.55 1.69-4.9L16.9 18.31C15.55 19.37 13.85 20 12 20zm6.31-3.1L7.1 5.69C8.45 4.63 10.15 4 12 4c4.42 0 8 3.58 8 8 0 1.85-.63 3.55-1.69 4.9z" />
+                  </svg>
+                </Fab>
+              )}
+            </p>
+          </CardContent>
+        </Card>
       </div>
     );
   }
