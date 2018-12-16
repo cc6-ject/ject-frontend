123407b17e1156a0fe7084fd9e7f231c55ff7b18
egurinko <egctoru@gmail.com>
2018-12-16 23:15:25 +0900

    Added transcript feature to activity.

--- src/Activity.jsx
+++ src/Activity.jsx
@@ -1,26 +1,13 @@
 import React from 'react';
 import { API } from 'aws-amplify';
 import Chart from 'chart.js';
+import 'chartjs-plugin-annotation';
 import moment from 'moment';
 import Button from '@material-ui/core/Button';
 import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
+import { NAME_OF_MONTH, NUM_OF_DAYS } from './Constants';
 import './Activity.css';
-
-const numOfDays = {
-  1: 31,
-  2: 28,
-  3: 31,
-  4: 30,
-  5: 31,
-  6: 30,
-  7: 31,
-  8: 31,
-  9: 30,
-  10: 31,
-  11: 30,
-  12: 31
-};
 
 const NOW = new Date();
 const DATE = NOW.getDate();
@@ -29,7 +16,6 @@ const YEAR = NOW.getFullYear();
 const PRE_MONTH = MONTH - 1 > 0 ? MONTH - 1 : MONTH + 11;
 const PRE_PRE_MONTH = MONTH - 2 > 0 ? MONTH - 2 : MONTH + 10;
 const PRE_PRE_PRE_MONTH = MONTH - 3 > 0 ? MONTH - 3 : MONTH + 9;
-console.log(YEAR, MONTH, DATE);
 
 class Activity extends React.Component {
   constructor(props) {
@@ -38,7 +24,7 @@ class Activity extends React.Component {
       projectionData: null,
       karaokeData: null,
       TTData: null,
-      view: 'Days',
+      view: 'Day',
       activity: 'Projection',
       timeAnchorEl: null,
       activityAnchorEl: null
@@ -49,9 +35,9 @@ class Activity extends React.Component {
     try {
       const data = await this.getDecibel();
       const proData = this.arrangeData(data, 'projection');
-      this.setState({ projectionData: proData });
-      const { projectionData, activity } = this.state;
-      this.renderDays(projectionData, activity);
+      await this.setState({ projectionData: proData });
+      const { projectionData } = this.state;
+      this.renderDay(projectionData);
     } catch (error) {
       console.log(error);
     }
@@ -79,7 +65,8 @@ class Activity extends React.Component {
         createdAt: moment(val.createdAt),
         avgDecibel: val.avgDecibel,
         duration: val.duration,
-        decibels: JSON.parse(val.decibels)
+        decibels: JSON.parse(val.decibels),
+        transcripts: val.transcripts ? JSON.parse(val.transcripts) : undefined
       }));
     }
     if (type === 'karaoke') {
@@ -104,32 +91,78 @@ class Activity extends React.Component {
     }));
   };
 
-  renderDays = (data, activityType) => {
+  renderActivity = data => {
     const result = [];
     const scale = [];
-    const days = numOfDays[`${MONTH}`];
+    for (let i = 0; i <= data.duration; i += 0.5) {
+      scale.push(i);
+      result.push({
+        transcripts: data.transcripts,
+        x: i,
+        y: data.decibels[i * 2]
+      });
+    }
 
-    // eslint-disable-next-line no-plusplus
-    for (let i = 1; i <= days; i++) {
+    this.drawBarChart(scale, result, 'line');
+  };
+
+  renderDay = (data, date = DATE) => {
+    const result = [];
+    const scale = [];
+    const { activity } = this.state;
+
+    const dailyData = data.filter(training => {
+      const { createdAt } = training;
+      return (
+        createdAt.year() === YEAR &&
+        createdAt.month() + 1 === MONTH &&
+        createdAt.date() === date
+      );
+    });
+
+    dailyData.forEach((training, i) => {
+      const { createdAt } = training;
+      const hour = createdAt.hours();
+      const numMin = createdAt.minutes();
+      const min =
+        numMin.toString().length === 1
+          ? `0${createdAt.minutes()}`
+          : createdAt.minutes();
+      scale.push(`${hour}:${min}`);
+      if (activity === 'Projection' || activity === 'Karaoke') {
+        result.push({ x: i, y: training.avgDecibel });
+      } else {
+        result.push({ x: i, y: training.avgDecibel });
+      }
+    });
+    this.drawBarChart(scale, result, 'bar');
+  };
+
+  renderDays = data => {
+    const result = [];
+    const scale = [];
+    const days = NUM_OF_DAYS[`${MONTH}`];
+    const { activity } = this.state;
+
+    const monthlyData = data.filter(training => {
+      const { createdAt } = training;
+      return createdAt.year() === YEAR && createdAt.month() + 1 === MONTH;
+    });
+
+    for (let i = 1; i <= days; i += 1) {
       scale.push(i);
       result.push({ x: `${i}`, y: 0 });
     }
 
-    const monthlyData = data.filter(activity => {
-      const { createdAt } = activity;
-      return createdAt.year() === YEAR && createdAt.month() + 1 === MONTH;
-    });
-
     result.forEach(day => {
       let count = 0;
       // eslint-disable-next-line no-restricted-syntax
-      for (const activity of monthlyData) {
-        if (Number(day.x) === activity.createdAt.date()) {
-          if (activityType === 'Projection' || activityType === 'Karaoke') {
-            day.y += activity.avgDecibel;
+      for (const training of monthlyData) {
+        if (Number(day.x) === training.createdAt.date()) {
+          if (activity === 'Projection' || activity === 'Karaoke') {
+            day.y += training.avgDecibel;
           } else {
-            // For Tongue Twister
-            // day.y += activity.avgDecibel;
+            day.y += training.coverage;
           }
           count += 1;
         }
@@ -138,17 +171,17 @@ class Activity extends React.Component {
         day.y /= count;
       }
     });
-    console.log(scale, result);
 
-    this.drawBarChart(scale, result);
+    this.drawBarChart(scale, result, 'bar');
   };
 
-  renderWeeks = (data, activityType) => {
+  renderWeeks = data => {
     const chartItem = this.makeWeekScale();
     const { result, scale } = chartItem;
+    const { activity } = this.state;
 
-    const threeMonthsData = data.filter(activity => {
-      const { createdAt } = activity;
+    const threeMonthsData = data.filter(training => {
+      const { createdAt } = training;
       return (
         (createdAt.year() === YEAR || createdAt.year() === YEAR - 1) &&
         (createdAt.month() + 1 === MONTH ||
@@ -161,17 +194,16 @@ class Activity extends React.Component {
       let count = 0;
       const { date } = week;
       // eslint-disable-next-line no-restricted-syntax
-      for (const activity of threeMonthsData) {
-        const { createdAt } = activity;
+      for (const training of threeMonthsData) {
+        const { createdAt } = training;
         if (week.month === createdAt.month() + 1) {
           const inWeek =
             createdAt.date() <= date && createdAt.date() >= date - 6;
           if (inWeek) {
-            if (activityType === 'Projection' || activityType === 'Karaoke') {
-              week.y += activity.avgDecibel;
+            if (activity === 'Projection' || activity === 'Karaoke') {
+              week.y += training.avgDecibel;
             } else {
-              // For Tongue Twister
-              // day.y += activity.avgDecibel;
+              week.y += training.coverage;
             }
             count += 1;
           }
@@ -181,7 +213,7 @@ class Activity extends React.Component {
         week.y /= count;
       }
     });
-    this.drawBarChart(scale, result);
+    this.drawBarChart(scale, result, 'bar');
   };
 
   makeWeekScale = () => {
@@ -194,10 +226,10 @@ class Activity extends React.Component {
     for (let i = 0; i <= 11; i += 1) {
       const targetDay = baseDate - 7 * i;
       if (targetDay > 0) {
-        scale.unshift(`${MONTH} / ${targetDay}`);
+        scale.unshift(`${MONTH}/${targetDay}`);
         result.unshift({ month: MONTH, date: targetDay, y: 0 });
       } else if (targetDay > -30) {
-        days = numOfDays[`${PRE_MONTH}`];
+        days = NUM_OF_DAYS[`${PRE_MONTH}`];
         scale.unshift(`${PRE_MONTH}/${targetDay + days}`);
         result.unshift({
           month: PRE_MONTH,
@@ -205,7 +237,7 @@ class Activity extends React.Component {
           y: 0
         });
       } else if (targetDay > -60) {
-        days = numOfDays[`${PRE_MONTH}`] + numOfDays[`${PRE_PRE_MONTH}`];
+        days = NUM_OF_DAYS[`${PRE_MONTH}`] + NUM_OF_DAYS[`${PRE_PRE_MONTH}`];
         scale.unshift(`${PRE_PRE_MONTH}/${targetDay + days}`);
         result.unshift({
           month: PRE_PRE_MONTH,
@@ -214,9 +246,9 @@ class Activity extends React.Component {
         });
       } else {
         days =
-          numOfDays[`${PRE_MONTH}`] +
-          numOfDays[`${PRE_PRE_MONTH}`] +
-          numOfDays[`${PRE_PRE_PRE_MONTH}`];
+          NUM_OF_DAYS[`${PRE_MONTH}`] +
+          NUM_OF_DAYS[`${PRE_PRE_MONTH}`] +
+          NUM_OF_DAYS[`${PRE_PRE_PRE_MONTH}`];
         scale.unshift(`${PRE_PRE_PRE_MONTH}/${targetDay + days}`);
         result.unshift({
           month: PRE_PRE_PRE_MONTH,
@@ -228,69 +260,123 @@ class Activity extends React.Component {
     return { result, scale };
   };
 
-  renderMonths = (data, activityType) => {
+  renderMonths = data => {
     const result = [];
     const scale = [];
     for (let i = 1; i <= 12; i += 1) {
-      scale.push(i);
+      scale.push(NAME_OF_MONTH[i]);
       result.push({ x: `${i}`, y: 0 });
     }
 
     const annualData = data.filter(
-      activity => activity.createdAt.year() === YEAR
+      training => training.createdAt.year() === YEAR
     );
-
-    result.forEach(day => {
+    const { activity } = this.state;
+    result.forEach(month => {
       let count = 0;
       // eslint-disable-next-line no-restricted-syntax
-      for (const activity of annualData) {
-        if (Number(day.x) === activity.createdAt.month() + 1) {
-          if (activityType === 'Projection' || activityType === 'Karaoke') {
-            day.y += activity.avgDecibel;
+      for (const training of annualData) {
+        if (Number(month.x) === training.createdAt.month() + 1) {
+          if (activity === 'Projection' || activity === 'Karaoke') {
+            month.y += training.avgDecibel;
           } else {
-            // For Tongue Twister
-            // day.y += activity.avgDecibel;
+            month.y += training.coverage;
           }
           count += 1;
         }
       }
       if (count !== 0) {
-        day.y /= count;
+        month.y /= count;
       }
     });
-    this.drawBarChart(scale, result);
+    this.drawBarChart(scale, result, 'bar');
   };
 
-  drawBarChart = (scale, data) => {
+  drawBarChart = (scale, data, chartType) => {
     this.initBarChart();
+    const { activity, view } = this.state;
+    const yLabel = activity === 'Tongue Twister' ? '%' : 'dB';
+    const transparent = activity === 'Tongue Twister' ? 0 : 0.5;
+    const isLabel = activity !== 'Tongue Twister';
 
     const ctx = document.getElementById('primaryChart').getContext('2d');
-    // eslint-disable-next-line no-new
-    new Chart(ctx, {
-      type: 'bar',
+    const chart = new Chart(ctx, {
+      type: chartType,
       data: {
         labels: scale,
-        datasets: [{ data, backgroundColor: 'rgba(249, 170, 51, 0.6)' }]
+        datasets: [
+          {
+            data,
+            backgroundColor: 'rgba(249, 170, 51, 0.6)',
+            hoverBackgroundColor: 'rgba(249, 170, 51, 0.9)'
+          }
+        ]
       },
       options: {
         scales: {
           yAxes: [
             {
               ticks: { beginAtZero: true },
-              scaleLabel: { display: true, labelString: 'dB', fontSize: 18 }
+              scaleLabel: { display: true, labelString: yLabel, fontSize: 18 }
             }
           ],
           xAxes: [
             {
               barPercentage: 0.4,
+              barThickness: 10,
               ticks: { fontSize: 18 }
             }
           ]
         },
         legend: {
           display: false
-        }
+        },
+        annotation: {
+          annotations: [
+            {
+              type: 'line',
+              id: 'hLine',
+              mode: 'horizontal',
+              scaleID: 'y-axis-0',
+              value: 40,
+              borderWidth: 1,
+              borderColor: `rgba(20, 29, 33, ${transparent})`,
+              label: {
+                enabled: isLabel,
+                content: 'Proper Level'
+              }
+            }
+          ]
+        },
+        showAllTooltips: true,
+        tooltips: {
+          enabled: true,
+          mode: 'label',
+          backgroundColor: '#344955',
+          caretSize: 5,
+          callbacks: {
+            title(tooltipItem, data) {
+              return `${data.labels[tooltipItem[0].index]}Sec`;
+            },
+            label(tooltipItem, data) {
+              return `${data.datasets[0].data[tooltipItem.index].y}dB`;
+            },
+            afterBody(tooltipItem, data) {
+              if (activity === 'Projection' && view === 'Activity') {
+                const { index } = tooltipItem[0];
+                const { transcripts } = data.datasets[0].data[index];
+                const range = Math.floor(index / 20);
+                return transcripts ? transcripts[range] : '';
+              }
+              return '';
+            } // afterBody;
+          } // callbacks;
+        } // tooltips;
       }
+    });
+    const canvas = document.getElementById('primaryChart');
+    canvas.addEventListener('click', e => {
+      this.handleChartClick(e, chart);
     });
   };
 
@@ -301,8 +387,46 @@ class Activity extends React.Component {
     const newCanvas = document.createElement('canvas');
     newCanvas.setAttribute('id', 'primaryChart');
     newCanvas.setAttribute('height', '300');
-    newCanvas.setAttribute('width', '1200');
+    newCanvas.setAttribute('width', '1500');
     wrapper.append(newCanvas);
+  };
+
+  handleChartClick = async (e, chart) => {
+    const { view, activity, projectionData, karaokeData, TTData } = this.state;
+    if (view === 'Day') {
+      // Extract Time and Y data from clicked data
+      const { data } = chart.getElementsAtEvent(e)[0]._chart;
+      const { _index } = chart.getElementsAtEvent(e)[0];
+      const label = data.labels[_index].split(':');
+      const hour = Number(label[0]);
+      const minutes = Number(label[1]);
+      const YData = data.datasets[0].data[_index].y;
+
+      const allData =
+        activity === 'Projection'
+          ? projectionData
+          : activity === 'Karaoke'
+          ? karaokeData
+          : TTData;
+
+      const targetAct = allData.find(training => {
+        const { createdAt } = training;
+        return (
+          training.avgDecibel === YData &&
+          createdAt.year() === YEAR &&
+          createdAt.month() + 1 === MONTH &&
+          createdAt.date() === DATE &&
+          createdAt.hours() === hour &&
+          createdAt.minutes() === minutes
+        );
+      });
+      try {
+        await this.setState({ view: 'Activity' });
+        this.renderActivity(targetAct);
+      } catch (e) {
+        console.log(e);
+      }
+    }
   };
 
   handleTimeClick = event => {
@@ -315,52 +439,62 @@ class Activity extends React.Component {
     this.setState({ activityAnchorEl: target });
   };
 
-  handleTimeClose = e => {
+  handleTimeClose = async e => {
     const menuItem = e.currentTarget.textContent;
     const { activity, projectionData, karaokeData, TTData } = this.state;
+    const data =
+      activity === 'Projection'
+        ? projectionData
+        : activity === 'Karaoke'
+        ? karaokeData
+        : TTData;
 
-    let data;
-    if (activity === 'Projection') {
-      data = projectionData;
-    } else if (activity === 'Karaoke') {
-      data = karaokeData;
-    } else if (activity === 'Tongue Twister') {
-      data = TTData;
+    try {
+      await this.setState({ view: menuItem });
+      const { view } = this.state;
+      if (view === 'Day') {
+        this.renderDay(data);
+      } else if (view === 'Days') {
+        this.renderDays(data);
+      } else if (view === 'Weeks') {
+        this.renderWeeks(data);
+      } else {
+        this.renderMonths(data);
+      }
+    } catch (e) {
+      console.log(e);
     }
-
-    if (menuItem === 'Days') {
-      this.renderDays(data, activity);
-    } else if (menuItem === 'Weeks') {
-      this.renderWeeks(data, activity);
-    } else {
-      this.renderMonths(data, activity);
-    }
-
-    this.setState({ timeAnchorEl: null, view: menuItem });
+    this.setState({ timeAnchorEl: null });
   };
 
-  handleActivityClose = e => {
+  handleActivityClose = async e => {
     const menuItem = e.currentTarget.textContent;
-    const { activity, view, projectionData, karaokeData, TTData } = this.state;
+    const { view, projectionData, karaokeData, TTData } = this.state;
 
-    let data;
-    if (menuItem === 'Projection') {
-      data = projectionData;
-    } else if (menuItem === 'Karaoke') {
-      data = karaokeData;
-    } else {
-      data = TTData;
+    try {
+      await this.setState({ activity: menuItem });
+      const { activity } = this.state;
+      const data =
+        activity === 'Projection'
+          ? projectionData
+          : activity === 'Karaoke'
+          ? karaokeData
+          : TTData;
+      if (view === 'Day' || view === 'Activity') {
+        await this.setState({ view: 'Day' });
+        this.renderDay(data);
+      } else if (view === 'Days') {
+        this.renderDays(data);
+      } else if (view === 'Weeks') {
+        this.renderWeeks(data);
+      } else {
+        this.renderMonths(data);
+      }
+    } catch (e) {
+      console.log(e);
     }
 
-    if (view === 'Days') {
-      this.renderDays(data, activity);
-    } else if (view === 'Weeks') {
-      this.renderWeeks(data, activity);
-    } else {
-      this.renderMonths(data, activity);
-    }
-
-    this.setState({ activityAnchorEl: null, activity: menuItem });
+    this.setState({ activityAnchorEl: null });
   };
 
   render() {
@@ -380,6 +514,7 @@ class Activity extends React.Component {
           open={Boolean(timeAnchorEl)}
           onClose={this.handleTimeClose}
         >
+          <MenuItem onClick={this.handleTimeClose}>Day</MenuItem>
           <MenuItem onClick={this.handleTimeClose}>Days</MenuItem>
           <MenuItem onClick={this.handleTimeClose}>Weeks</MenuItem>
           <MenuItem onClick={this.handleTimeClose}>Months</MenuItem>
@@ -403,10 +538,8 @@ class Activity extends React.Component {
         </Menu>
         <div className="primaryChartWrapper">
           <div id="primaryChartAreaWrapper">
-            <canvas id="primaryChart" height="300" width="1200" />
+            <canvas id="primaryChart" onClick={() => this.handleChartClick()} />
           </div>
-
-          <canvas id="primaryChartAxis" height="300" width="0" />
         </div>
       </div>
     );
