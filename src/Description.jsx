0599ac804b2259bbf168071c51380f126e2dabaa
Daenam Kim <daenam.kim@gmail.com>
2018-12-21 10:58:07 +0900

    Basic  description dialog on karaoke.

---
+++ src/Description.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import {
+  Dialog,
+  DialogActions,
+  DialogContent,
+  // DialogContentText,
+  DialogTitle,
+  Button
+} from '@material-ui/core';
+
+const Description = ({ open, onClose }) => (
+  <Dialog open={open} scroll="paper">
+    <DialogTitle>Title</DialogTitle>
+    <DialogContent>Some description.</DialogContent>
+    <DialogActions>
+      <Button onClick={onClose} color="primary">
+        OK
+      </Button>
+    </DialogActions>
+  </Dialog>
+);
+
+export default Description;
